a116e4235e8747a04b481836e437cad47ad86fc1
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-01-11 20:52:24 +0100

    VO 2.1 - DriverLicense - wprowadzenie obiektu z testami jednostkowymi

---
+++ src/Cabs/Entity/DriverLicense.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LegacyFighter.Cabs.Entity;
+
+public class DriverLicense
+{
+  public const string DriverLicenseRegex = "^[A-Z9]{5}\\d{6}[A-Z9]{2}\\d[A-Z]{2}$";
+
+  private readonly string _driverLicense;
+
+  private DriverLicense(string driverLicense)
+  {
+    _driverLicense = driverLicense;
+  }
+
+  public static DriverLicense WithLicense(string driverLicense)
+  {
+    if (string.IsNullOrEmpty(driverLicense) || !Regex.IsMatch(driverLicense, DriverLicenseRegex))
+    {
+      throw new ArgumentException("Illegal license no = " + driverLicense);
+    }
+
+    return new DriverLicense(driverLicense);
+  }
+
+  public static DriverLicense WithoutValidation(string driverLicense)
+  {
+    return new DriverLicense(driverLicense);
+  }
+
+  public override string ToString()
+  {
+    return "DriverLicense{" +
+           "driverLicense='" + _driverLicense + '\'' +
+           '}';
+  }
+
+  public string AsString()
+  {
+    return _driverLicense;
+  }
+}
