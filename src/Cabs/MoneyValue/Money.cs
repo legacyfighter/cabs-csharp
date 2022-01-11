118e23f501d85446d76409bf5e5e695672b59041
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-01-11 20:52:26 +0100

    VO 2.2 - Money - wprowadzenie VO wraz z testami jednostkowymi

---
+++ src/Cabs/MoneyValue/Money.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+
+namespace LegacyFighter.Cabs.MoneyValue;
+
+public class Money : IEquatable<Money>
+{
+  public static readonly Money Zero = new(0);
+  private readonly int _value;
+
+  public Money(int value)
+  {
+    _value = value;
+  }
+
+  public static Money operator +(Money money, Money other)
+  {
+    return new Money(money._value + other._value);
+  }
+
+  public static Money operator -(Money money, Money other)
+  {
+    return new Money(money._value - other._value);
+  }
+
+  public Money Percentage(int percentage)
+  {
+    return new Money((int)Math.Round(percentage * _value / 100.0));
+  }
+
+  public int ToInt()
+  {
+    return _value;
+  }
+
+  public bool Equals(Money other)
+  {
+    if (ReferenceEquals(null, other)) return false;
+    if (ReferenceEquals(this, other)) return true;
+    return _value == other._value;
+  }
+
+  public override bool Equals(object obj)
+  {
+    if (ReferenceEquals(null, obj)) return false;
+    if (ReferenceEquals(this, obj)) return true;
+    if (obj.GetType() != GetType()) return false;
+    return Equals((Money)obj);
+  }
+
+  public override int GetHashCode()
+  {
+    return _value;
+  }
+
+  public static bool operator ==(Money left, Money right)
+  {
+    return Equals(left, right);
+  }
+
+  public static bool operator !=(Money left, Money right)
+  {
+    return !Equals(left, right);
+  }
+
+  public override string ToString()
+  {
+    var value = (double)_value / 100;
+    return value.ToString("0.00", CultureInfo.CreateSpecificCulture("en-US"));
+  }
+}
---
+++ src/CabsTests/MoneyValue/MoneyTest.cs
@@ -0,0 +1,56 @@
+﻿using LegacyFighter.Cabs.MoneyValue;
+
+namespace LegacyFighter.CabsTests.MoneyValue;
+
+public class MoneyTest
+{
+  [Test]
+  public void CanCreateMoneyFromInteger()
+  {
+    //expect
+    Assert.AreEqual("100.00", new Money(10000).ToString());
+    Assert.AreEqual("0.00", new Money(0).ToString());
+    Assert.AreEqual("10.12", new Money(1012).ToString());
+  }
+
+  [Test]
+  public void ShouldProjectMoneyToInteger()
+  {
+    //expect
+    Assert.AreEqual(10, new Money(10).ToInt());
+    Assert.AreEqual(0, new Money(0).ToInt());
+    Assert.AreEqual(-5, new Money(-5).ToInt());
+  }
+
+  [Test]
+  public void CanAddMoney()
+  {
+    //expect
+    Assert.AreEqual(new Money(1000), new Money(500) + new Money(500));
+    Assert.AreEqual(new Money(1042), new Money(1020) + new Money(22));
+    Assert.AreEqual(new Money(0), new Money(0) + new Money(0));
+    Assert.AreEqual(new Money(-2), new Money(-4) + new Money(2));
+  }
+
+  [Test]
+  public void CanSubtractMoney()
+  {
+    //expect
+    Assert.AreEqual(Money.Zero, new Money(50) - new Money(50));
+    Assert.AreEqual(new Money(998), new Money(1020) - new Money(22));
+    Assert.AreEqual(new Money(-1), new Money(2) - new Money(3));
+  }
+
+  [Test]
+  public void CanCalculatePercentage()
+  {
+    //expect
+    Assert.AreEqual("30.00", new Money(10000).Percentage(30).ToString());
+    Assert.AreEqual("26.40", new Money(8800).Percentage(30).ToString());
+    Assert.AreEqual("88.00", new Money(8800).Percentage(100).ToString());
+    Assert.AreEqual("0.00", new Money(8800).Percentage(0).ToString());
+    Assert.AreEqual("13.20", new Money(4400).Percentage(30).ToString());
+    Assert.AreEqual("0.30", new Money(100).Percentage(30).ToString());
+    Assert.AreEqual("0.00", new Money(1).Percentage(40).ToString());
+  }
+}
